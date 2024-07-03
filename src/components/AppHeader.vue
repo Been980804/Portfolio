45534eb2f6c1b348553d8bd4c9748f7335b04a24
hblee <hyeonbeen980804@gmail.com>
2024-07-03 13:10:15 +0900

    기본 틀 제작

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    <div class="logo">
+      <button class="logo_btn">
+        <img src="../assets/img/logo.png" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  @import "../assets/css/header.css";
+</style>

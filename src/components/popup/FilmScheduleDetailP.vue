470dfe7525bf80b62c2936a522f63717e1a3a3d0
hblee <hyeonbeen980804@gmail.com>
2024-07-17 09:02:08 +0900

    film detail 추가

--- src/components/popup/FilmScheduleDetailP.vue
+++ src/components/popup/FilmScheduleDetailP.vue
@@ -9,7 +9,7 @@
           <button @click="prevImage" class="arrow left-arrow">&lt;</button>
           <div class="detail_img_box_title">
             <strong>{{ currentTitle }}</strong>
-            <img :src="currentImage" class="detail_img">
+            <img :src="currentImage" class="detail_img" />
           </div>
           <button @click="nextImage" class="arrow right-arrow">&gt;</button>
         </div>
@@ -27,12 +27,15 @@
 <script>
 import headerImg from "@/assets/img/project/film/header.png";
 import mainBannerImg from "@/assets/img/project/film/main_banner.png";
+import footerImg from "@/assets/img/project/film/footer.png";
+import mainMovieImg from "@/assets/img/project/film/main_movieList.png";
+import movieDetailImg from "@/assets/img/project/film/movie_detail.png";
 
 export default {
   data() {
     return {
-      images: [headerImg, mainBannerImg],
-      titles: ["메인화면", "배너"],
+      images: [headerImg, footerImg, mainBannerImg, mainMovieImg, movieDetailImg],
+      titles: ["Header", "Footer", "배너", "현재상영작, 상영예정작 Top5", "영화 상세보기"],
       explanations: [
         [
           {
@@ -58,12 +61,53 @@ export default {
         ],
         [
           {
-            title: "배너 이미지",
-            detail: "배너 이미지는 주요 홍보 내용을 보여줍니다.",
+            title: "1. 공지사항 게시글",
+            detail: "각 공지사항의 상세보기 페이지로 이동합니다.",
           },
           {
-            title: "배너 설명",
-            detail: "배너 설명은 배너 이미지의 부가 설명입니다.",
+            title: "2. 공지사항 페이지",
+            detail: "공지사항 게시글 페이지로 이동합니다.",
+          },
+          {
+            title: "3. 문의사항 게시글",
+            detail: "각 문의사항의 상세보기 페이지로 이동합니다.",
+          },
+          {
+            title: "4. 문의사항 페이지",
+            detail: "문의사항 게시글 페이지로 이동합니다.",
+          },
+        ],
+        [
+          {
+            title: "1. 배너 이미지",
+            detail: "배너 이미지 클릭시 영화정보 상세보기 페이지로 이동합니다.",
+          },
+          {
+            title: "2. < > 버튼",
+            detail:
+              "박스오피스 영화 순위별로 5개 영화를 배너에 띄우고 < > 버튼으로 배너를 넘길 수 있습니다.",
+          },
+        ],
+        [
+          {
+            title: "1. 현재 상영작 Top5",
+            detail: "각 영화포스터 클릭시 해당 영화 상세정보 페이지로 이동합니다.",
+          },
+          {
+            title: "2. 상영 예정작 Top5",
+            detail:
+              "각 영화포스터 클릭시 해당 영화 상세정보 페이지로 이동합니다.",
+          },
+        ],
+        [
+          {
+            title: "1. 상영 시간표 비교",
+            detail: "현 영화의 상영 시간표를 비교하는 모달을 띄웁니다.",
+          },
+          {
+            title: "2. 상영 예정작 Top5",
+            detail:
+              "각 영화포스터 클릭시 해당 영화 상세정보 페이지로 이동합니다.",
           },
         ],
       ],
